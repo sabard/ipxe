b9d8c18d13c10539361b7d9f49fe6e7fe43a5f6a
Sabar Dasgupta <s@bardasgupta.com>
2022-12-05 03:00:47 +0000

    enable reboot and poweroff defaults by default. add easy compile script

--- src/config/general.h
+++ src/config/general.h
@@ -145,8 +145,8 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
 //#define LOTEST_CMD		/* Loopback testing commands */
 //#define VLAN_CMD		/* VLAN commands */
 //#define PXE_CMD		/* PXE commands */
-//#define REBOOT_CMD		/* Reboot command */
-//#define POWEROFF_CMD		/* Power off command */
+#define REBOOT_CMD		/* Reboot command */
+#define POWEROFF_CMD		/* Power off command */
 //#define IMAGE_TRUST_CMD	/* Image trust management commands */
 //#define PCI_CMD		/* PCI commands */
 //#define PARAM_CMD		/* Form parameter commands */
